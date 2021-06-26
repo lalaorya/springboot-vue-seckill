768c7d81f1b74c1b3bcf73dd3b5099e450144598
sang_Mu <sangmu.hhj@gmail.com>
2021-06-26 12:53:36 +0800

    添加了websoucket的依赖

---
+++ seckill-api/src/main/java/com/hhj/seckill/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,6 @@
+package com.hhj.seckill.common.interceptor;/**
+ * @Author virtual
+ * @Date 2021/6/25 18:12
+ * @Version 1.0
+ */public class LoginInterceptor {
+}
---
+++ seckill-api/src/main/java/com/hhj/seckill/config/LoginInterceptorConfig.java
@@ -0,0 +1,6 @@
+package com.hhj.seckill.config;/**
+ * @Author virtual
+ * @Date 2021/6/25 18:14
+ * @Version 1.0
+ */public class LoginInterceptorConfig {
+}
