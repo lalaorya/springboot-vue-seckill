44421dbeb16c68a3b481ac1442711e2a9032f48b
sang_Mu <sangmu.hhj@gmail.com>
2021-06-26 12:55:56 +0800

    拦截器配置类

--- seckill-api/src/main/java/com/hhj/seckill/config/LoginInterceptorConfig.java
+++ seckill-api/src/main/java/com/hhj/seckill/config/LoginInterceptorConfig.java
@@ -1,6 +1,26 @@
-package com.hhj.seckill.config;/**
+package com.hhj.seckill.config;
+
+import com.hhj.seckill.common.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
  * @Author virtual
  * @Date 2021/6/25 18:14
  * @Version 1.0
- */public class LoginInterceptorConfig {
+ */
+@Configuration
+public class LoginInterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LoginInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/**").excludePathPatterns("/login","/getCaptcha");
+
+    }
 }
