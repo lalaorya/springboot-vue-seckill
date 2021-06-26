8d3880b5b8fa970fb2cbc53fd556f96b2e6e9c21
sang_Mu <sangmu.hhj@gmail.com>
2021-06-26 12:55:43 +0800

    配置了允许暴露uuid请求头

--- seckill-api/src/main/java/com/hhj/seckill/config/CorsConfig.java
+++ seckill-api/src/main/java/com/hhj/seckill/config/CorsConfig.java
@@ -11,17 +11,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // 这里要用allowedOriginPatterns，不能用allowedOrigin，不然会报错
 //                .allowedOriginPatterns("*")
                 .allowedOrigins("*")
+                .allowCredentials(true)
                 .allowedMethods("*")
                 .allowCredentials(true)
+                .exposedHeaders("uuid")
                 .maxAge(3600)
                 .allowedHeaders("*");
     }
+
+
 
 
 
